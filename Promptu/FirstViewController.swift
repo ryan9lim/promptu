67108829168a7af2b9d3264b43d45535adec8de6
Ryan Lim <Limmy@ryans-mac-mini.local>
2016-08-01 23:12:43 -0400

    Initial Commit

---
+++ Promptu/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Promptu
+//
+//  Created by Ryan Lim on 8/1/16.
+//  Copyright © 2016 DailyGummies. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
